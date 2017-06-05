98d8d44990a7d419af8e168058118aa8e7e72141
boilercoding <boilercoding@gmail.com>
2017-06-05 22:54:52 +0000

    Adds cowboy dependency

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule StaticBlog.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :cowboy],
      mod: {StaticBlog.Application, []}]
   end
 
@@ -29,6 +29,6 @@ defmodule StaticBlog.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:cowboy, "~> 1.1"}]
   end
 end
