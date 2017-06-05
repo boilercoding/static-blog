f114b2c5c4a697a22bd44064b2c42b7df468656f
boilercoding <boilercoding@gmail.com>
2017-06-05 22:48:36 +0000

    Initial Commit

---
+++ lib/static_blog.ex
@@ -0,0 +1,18 @@
+defmodule StaticBlog do
+  @moduledoc """
+  Documentation for StaticBlog.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StaticBlog.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/static_blog_test.exs
@@ -0,0 +1,8 @@
+defmodule StaticBlogTest do
+  use ExUnit.Case
+  doctest StaticBlog
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
